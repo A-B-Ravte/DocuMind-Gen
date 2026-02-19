4a6798265ec28dcc437f77e17d9a544ce714876a
A-B-Ravte <letsmailravte@gmail.com>
2026-02-19 19:20:27 +0530

    Getting bonding box logic updated.

--- main.py
+++ main.py
@@ -7,11 +7,20 @@ from utility import DocOperation
 doc_ops = DocOperation()
 load_dotenv()
 
+class Fields(BaseModel):
+    BoundingBox : list[int] = Field(..., description="Get bBounding box of where the information found.")
+    Page_No : int = Field(..., description="Get page number where there information found")
+
+class NameField(Fields):
+    value : str = Field(..., description="Search for Name provided at the start of document.")
+
+class AddressField(Fields):
+    value : str = Field(..., description="Address provided at the start below the Name.")
+    
 class InvoiceModel(BaseModel):
-    Name : str = Field(..., description="Name provided at the start of document.")
-    Address : str = Field(..., description="Address provided at the start below the Name.")
-    BoundingBox : list[int] = Field(..., description="Bounding box of where the information found.")
-    Page_No : int = Field(..., description="page number where there information found")
+    Name : NameField
+    Address : AddressField
+    
 
 pdf_path = r'D:\aakash\SwitchFocus\Own_Project\DocuMind-Gen\Sample-Doc\Alfa_1.jpg'
 
@@ -21,8 +30,7 @@ if not doc_ops.is_digital_native(pdf_path = pdf_path):
     prompt = """"
     Extract the multi page document and get the Name and Address fields, 
     Document provided can have single page or multi page both in jpg, jpeg, png or pdf formats
-    Return list of json data per page vise that matches the Provided Schema.
-    Give me the bonding ractangles
+    Return json properly that matches the Provided Schema.
     """
 
     document = client.files.upload(file = pdf_path)
