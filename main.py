e29f02d8a45a5e8fbb3be0c77edb0606f400dcb2
A-B-Ravte <letsmailravte@gmail.com>
2026-02-19 19:09:25 +0530

    Integrated is-digital part in main.py

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import os
 from dotenv import load_dotenv
 from google import genai
 from pydantic import BaseModel, Field
+from utility import DocOperation
 
+doc_ops = DocOperation()
 load_dotenv()
 
 class InvoiceModel(BaseModel):
@@ -11,25 +13,31 @@ class InvoiceModel(BaseModel):
     BoundingBox : list[int] = Field(..., description="Bounding box of where the information found.")
     Page_No : int = Field(..., description="page number where there information found")
 
-client = genai.Client()
+pdf_path = r'D:\aakash\SwitchFocus\Own_Project\DocuMind-Gen\Sample-Doc\Alfa_1.jpg'
 
-prompt = """"
-Extract the multi page document and get the Name and Address fields, 
-Document provided can have single page or multi page both in jpg, jpeg, png or pdf formats
-Return list of json data per page vise that matches the Provided Schema.
-Give me the bonding ractangles
-"""
+if not doc_ops.is_digital_native(pdf_path = pdf_path):
+    client = genai.Client()
 
-document = client.files.upload(file = r'Sample-Doc\Alfa_1.jpg')
-response = client.models.generate_content(
-    model = 'gemini-2.5-flash',
-    contents = [document, prompt],
-    config={
-        'response_mime_type' : 'application/json',
-        'response_schema' : InvoiceModel
-    }
+    prompt = """"
+    Extract the multi page document and get the Name and Address fields, 
+    Document provided can have single page or multi page both in jpg, jpeg, png or pdf formats
+    Return list of json data per page vise that matches the Provided Schema.
+    Give me the bonding ractangles
+    """
 
-)
+    document = client.files.upload(file = pdf_path)
+    response = client.models.generate_content(
+        model = 'gemini-2.5-flash',
+        contents = [document, prompt],
+        config={
+            'response_mime_type' : 'application/json',
+            'response_schema' : InvoiceModel
+        }
 
-doc = InvoiceModel.model_validate_json(response.text)
-print(doc.model_dump())
+    )
+
+    doc = InvoiceModel.model_validate_json(response.text)
+    print(doc.model_dump())
+else :
+    pass # if pdf is digital then save cost of api and go by traditional way.
+
